62d37507269897cda50e93251510fa43225b86b6
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-02-16 09:50:30 +0200

    LINQ – Transformation Operators - Q5

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -109,7 +109,27 @@ namespace Demo
             //--------------------------------------------------------------------------------//
 
 
-         
+            ////5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+
+
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            //var pairs = from a in numbersA
+            //            from b in numbersB
+            //            where a < b
+            //            select new { A = a, B = b };
+
+            //foreach (var pair in pairs)
+            //{
+            //    Console.WriteLine($"{pair.A} is less than {pair.B}");
+            //}
+
+
+            //--------------------------------------------------------------------------------//
+
+        
+
 
             #endregion
 
